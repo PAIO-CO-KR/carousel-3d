3fc6de4a32008f6b6ba9194ddd6511a13da0249a
ChoiKyu Woo <apple@paio.co.kr>
2015-03-11 03:20:43 +0900

    child resize.

--- src/child.js
+++ src/child.js
@@ -9,25 +9,63 @@
   "use strict";
 
   var $ = window.jQuery;
-  //var Modernizr = window.Modernizr;
+  var Modernizr = window.Modernizr;
 
-  var Child = function (childrenWrapperObj, childContent) {
+  var Child = function (childrenWrapperObj, content) {
     this._childrenWrapperObj = childrenWrapperObj;
-    this._childContent = childContent;
+    this._content = content;
 
-    var wrapper = $('<div data-child-wrapper />')[0];
-    this._childWrapper = this.el = wrapper;
-    $(wrapper).append(childContent);
+    //manipulate dome
+    this.el = $('<div data-child />')[0];
+    this._contentWrapper = $('<div data-content-wrapper />')[0];
+    $(this.el).append(this._contentWrapper);
+    $(this._contentWrapper).append(content);
+
+    this._hideUntilLoad();
   };
 
 
   Child.prototype._childrenWrapperObj = null;
 
-  Child.prototype._chlidContent = null;
+  Child.prototype._content = null;
 
   Child.prototype.el = null;
 
-  Child.prototype._cihldWrapper = null;
+  Child.prototype._contentWrapper = null;
+
+  Child.prototype._hideUntilLoad = function () {
+    $(this._content).css('visibility', 'hidden');
+    $(this._contentWrapper).waitForImages(function () {
+      $(this._content).resize(this._resize.bind(this));
+      setTimeout(function () {
+        this._resize();
+        $(this._content).css('visibility', 'visible');
+      }.bind(this), 1);
+    }.bind(this));
+  };
+
+  Child.prototype._resize = function () {
+    $(this._contentWrapper).width($(this._content).outerWidth());
+    $(this._contentWrapper).height($(this._content).outerHeight());
+
+    var horizontalScale = $(this.el).width() / $(this._contentWrapper).outerWidth();
+    var verticalScale = $(this.el).height() / $(this._contentWrapper).outerHeight();
+    var scale = Math.min(horizontalScale, verticalScale);
+    var horizontalOffset = Math.floor(($(this.el).width() - ($(this._contentWrapper).outerWidth() * scale)) / 2);
+    var verticalOffset = Math.floor(($(this.el).height() - ($(this._contentWrapper).outerHeight() * scale)) / 2);
+
+    $(this.el).css('padding-left', horizontalOffset + 'px');
+    $(this.el).css('padding-top', verticalOffset + 'px');
+    if (Modernizr.csstransforms) {
+      $(this._contentWrapper).css('transform', 'scale(' + scale + ')');
+      $(this._contentWrapper).css('-ms-transform', 'scale(' + scale + ')');
+      $(this._contentWrapper).css('-moz-transform', 'scale(' + scale + ')');
+      $(this._contentWrapper).css('-webkit-transform', 'scale(' + scale + ')');
+    } else {
+      $(this._contentWrapper).css('filter', 'progid:DXImageTransform.Microsoft.Matrix(M11=' + scale + ', M12=0, M21=0, M22=' + scale + ', SizingMethod="auto expand")');
+      $(this._contentWrapper).css('-ms-filter', 'progid:DXImageTransform.Microsoft.Matrix(M11=' + scale + ', M12=0, M21=0, M22=' + scale + ', SizingMethod="auto expand")');
+    }
+  };
 
   module.exports = Child;
 })();
